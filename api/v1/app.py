ad1d25c1e5d254f31a02f73f3dd8b927d3777d14
francis <krypton@gmail.com>
2024-11-15 14:32:52 +0100

    Add custom 403 error handler and test point

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from typing import Literal
 
 
 app = Flask(__name__)
@@ -20,6 +20,19 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler.
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden handler.
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
